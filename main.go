46e876f85b7416593ef2154a74fa62a1b3fc3f0b
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-01-04 16:59:39 -0700

    Test variables, learn arrays and slices

--- main.go
+++ main.go
@@ -3,5 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("Hello Go World!")
+	var shortGolang = "Watch Go crash course"
+	var fullGolang = "Watch Nana's Golang Full Course"
+	var rewardDessert = "Reward myself with a cheesecake"
+
+	taskItems := []string{shortGolang, fullGolang, rewardDessert}
+
+	fmt.Println("###### Welcome to our Todolist APP! ######")
+
+	fmt.Println("List of my Todos")
+	fmt.Println("Tasks:", taskItems)
 }
