f7870a89b1386e4447661059a6d1dd0cd12cdd4f
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-01-04 17:29:12 -0700

    Test functions and learn variable scope

--- main.go
+++ main.go
@@ -3,14 +3,18 @@ package main
 import "fmt"
 
 func main() {
+	fmt.Println("###### Welcome to our Todolist APP! ######")
+
 	var shortGolang = "Watch Go crash course"
 	var fullGolang = "Watch Nana's Golang Full Course"
 	var rewardDessert = "Reward myself with a cheesecake"
 
-	taskItems := []string{shortGolang, fullGolang, rewardDessert}
+	var taskItems = []string{shortGolang, fullGolang, rewardDessert}
 
-	fmt.Println("###### Welcome to our Todolist APP! ######")
+	printTasks(taskItems)
+}
 
+func printTasks(taskItems []string) {
 	fmt.Println("List of my Todos")
 
 	for index, task := range taskItems {
